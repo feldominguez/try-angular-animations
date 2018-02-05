64e8bb7dadd52d5aad301d3b9de269ed5e64ce6d
Phil D. JP <felips.110@gmail.com>
2018-02-05 01:25:39 -0800

    adding stagger animation

--- src/app/stagger/stagger.component.ts
+++ src/app/stagger/stagger.component.ts
@@ -1,26 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef
+} from '@angular/core';
 
 import { RedditDataService } from '../shared';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  query,
+  stagger
+} from '@angular/animations';
+
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-stagger',
   templateUrl: './stagger.component.html',
-  styleUrls: ['./stagger.component.scss']
+  styleUrls: ['./stagger.component.scss'],
+  animations: [
+    trigger('listStagger', [
+      transition('* <=> *', [
+        query(
+          ':enter',
+          [
+              style({ opacity: 0, transform: 'translateY(-15px)' }),
+              stagger('50ms', animate(
+                '550ms ease-out',
+                style({ opacity: 1, transform: 'translateY(0px)' })
+              ))
+            ], { optional: true }
+        ),
+        query(
+          ':leave',
+          animate('50ms', style({ opacity: 0 })),
+          { optional: true }
+        )
+      )
+  ]
 })
+
 export class StaggerComponent implements OnInit {
   asyncSubreddits;
   staticSubreddits;
 
-  constructor(private redditData: RedditDataService) {}
+  asyncDataLength: number;
+
+  constructor(
+    private redditData: RedditDataService,
+    private cd: ChangeDetectorRef
+  ) {}
 
   ngOnInit() {
-    this.redditData
-      .getData()
-      .map((data: any) => data.subreddit_names)
-      .subscribe(data => (this.asyncSubreddits = data));
+    this.asyncSubreddits = this.redditData.getData().map((data: any) => {
+      this.asyncDataLength = data.subreddit_names.length;
+      return data.subreddit_names;
+    });
 
     this.redditData
       .getStaticData()
@@ -28,21 +70,30 @@ export class StaggerComponent implements OnInit {
       .subscribe(data => (this.staticSubreddits = data));
   }
 
+  clearStaticSubReddits(): void {
+    this.staticSubreddits = [];
+  }
+
   reloadAsyncSubreddits(): void {
     this.asyncSubreddits = null;
-    this.redditData
-      .getData()
-      .map((data: any) => data.subreddit_names)
-      .subscribe(data => (this.asyncSubreddits = data));
+    this.asyncDataLength = null;
+    this.asyncSubreddits = this.redditData.getData().map((data: any) => {
+      this.asyncDataLength = data.subreddit_names.length;
+      return data.subreddit_names;
+    });
   }
 
   reloadStaticSubreddits(): void {
-    this.staticSubreddits = null;
+    this.clearStaticSubReddits();
+    this.cd.detectChanges();
+
     this.redditData
       .getStaticData()
       .map((data: any) => data.subreddit_names)
       .subscribe(data => {
+        console.log(this.staticSubreddits);
         this.staticSubreddits = data;
+        this.cd.detectChanges();
       });
   }
 }
