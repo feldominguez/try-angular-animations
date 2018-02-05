c41196330568b4d1d0ae3f0ab08791374dda1100
Phil D. JP <felips.110@gmail.com>
2018-02-05 01:35:06 -0800

    fixing stagger component

--- src/app/stagger/stagger.component.ts
+++ src/app/stagger/stagger.component.ts
@@ -31,22 +31,24 @@ import {
         query(
           ':enter',
           [
-              style({ opacity: 0, transform: 'translateY(-15px)' }),
-              stagger('50ms', animate(
+            style({ opacity: 0, transform: 'translateY(-15px)' }),
+            stagger(
+              '50ms',
+              animate(
                 '550ms ease-out',
                 style({ opacity: 1, transform: 'translateY(0px)' })
-              ))
-            ], { optional: true }
-        ),
-        query(
-          ':leave',
-          animate('50ms', style({ opacity: 0 })),
+              )
+            )
+          ],
           { optional: true }
-        )
-      )
+        ),
+        query(':leave', animate('50ms', style({ opacity: 0 })), {
+          optional: true
+        })
+      ])
+    ])
   ]
 })
-
 export class StaggerComponent implements OnInit {
   asyncSubreddits;
   staticSubreddits;
